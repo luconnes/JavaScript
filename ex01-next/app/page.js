1a17d0319ad32d289d83c7e7d366b02ee51a2744
Lucas Ribeiro D'Azevedo <128042824+luconnes@users.noreply.github.com>
2025-08-18 23:24:09 -0300

    correcao

--- ex01-next/app/page.js
+++ ex01-next/app/page.js
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>ola turma<h1>
-      <p>Teste Paragrafação<p>
+      <h1>ola turma</h1>
+      <p>Teste Paragrafação</p>
       <p>Teste Novamente</p>
     </div>
   );
