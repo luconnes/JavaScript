0684db4457b26e508649e99a567e7c91d6123bf6
Lucas Ribeiro D'Azevedo <128042824+luconnes@users.noreply.github.com>
2025-08-22 01:41:52 +0000

    commit atividade mini bio

--- ex01-next/app/page.js
+++ ex01-next/app/page.js
@@ -3,9 +3,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>ola turma</h1>
-      <p>Teste Paragrafação</p>
-      <p>Teste Novamente</p>
+      <h1>Lucas Ribeiro D'Azevedo</h1>
+      <hr/>
+      <img src="/assets/images/image.png"  width="500px"  height="500px"/>
+      <hr/>
+      <p>Sou estudante de Ciência da Computação, atualmente com experiência em programação (Python, C, Java, SQL), banco de dados e engenharia de software, incluindo metodologias ágeis e gestão de projetos. Também possuo conhecimentos em áreas correlatas como comunicação, UX, vendas e análise de processos, além de Excel intermediário/avançado e especialização em andamento em Power BI. Busco oportunidades para aplicar e expandir essas habilidades, contribuindo com soluções práticas, criativas e orientadas a resultados.</p>
+      
     </div>
   );
 }
